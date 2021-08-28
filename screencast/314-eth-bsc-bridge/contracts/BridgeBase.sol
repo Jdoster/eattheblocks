36c0b9aa5e82741a6001f2fdaf29ad7053d3773c
Jdoster <48856206+Jdoster@users.noreply.github.com>
2021-08-28 18:37:47 -0400

    Update BridgeBase.sol

--- screencast/314-eth-bsc-bridge/contracts/BridgeBase.sol
+++ screencast/314-eth-bsc-bridge/contracts/BridgeBase.sol
@@ -4,8 +4,8 @@ import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import './Itoken.sol';
 
 contract BridgeBase {
-  address public admin:'59b2e58f6796581839eb2423a339ff2e03d9360531ff1b050131bacbc24ed881';
-  IToken public token:'0xc778417e063141139fce010982780140aa0cd5ab';
+  address public admin'59b2e58f6796581839eb2423a339ff2e03d9360531ff1b050131bacbc24ed881';
+  IToken public token'0xc778417e063141139fce010982780140aa0cd5ab';
   uint public nonce;
   mapping(uint => bool) public processedNonces;
 
@@ -19,7 +19,7 @@ contract BridgeBase {
     Step indexed step
   );
 
-  constructor(address _token) {
+  constructor(address _token '0xc778417e063141139fce010982780140aa0cd5ab') {
     admin = msg.sender;
     token = IToken(_token);
   }
