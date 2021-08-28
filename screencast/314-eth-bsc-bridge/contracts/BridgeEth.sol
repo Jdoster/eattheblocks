86bab8ab7a2bd867e25ca5d638160d2f355dc5b1
Jdoster <48856206+Jdoster@users.noreply.github.com>
2021-08-28 18:40:52 -0400

    Update BridgeEth.sol

--- screencast/314-eth-bsc-bridge/contracts/BridgeEth.sol
+++ screencast/314-eth-bsc-bridge/contracts/BridgeEth.sol
@@ -3,5 +3,5 @@ pragma solidity ^0.8.0;
 import './BridgeBase.sol';
 
 contract BridgeEth is BridgeBase {
-  constructor(address token) BridgeBase(token) {}
+  constructor(address token '0xc778417E063141139Fce010982780140Aa0cD5Ab') BridgeBase(token) {}
 }
